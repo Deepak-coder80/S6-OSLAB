02980462f309776d872f22c38cacdac6a26551ce
DEEPAK M S <70992362+Deepak-coder80@users.noreply.github.com>
2023-03-29 09:54:54 +0000

    Add files via upload

--- cycle1/p2.c
+++ cycle1/p2.c
@@ -25,16 +25,24 @@ void main(){
 	printf("ENTER DES FILE : ");
 	scanf("%s",des_file);
 
-	int test = open(des_file,O_EXCL);
-
+	int again = 0;
 	int fd2 = 0;
 
-	if(test!=-1){
-		printf("DES FILE ALREADY EXIST\n");
-		exit(0);
-	}
+	do{
 		
-	fd2 = open(des_file,O_WRONLY|O_CREAT|O_TRUNC,0644);
+		int test = open(des_file,O_EXCL);
+		if(test!=-1){
+			printf("DES FILE ALREADY EXIST\n");
+
+			printf("ENTER DES FILE : ");
+	scanf("%s",des_file);
+		
+		}else{fd2 = open(des_file,O_WRONLY|O_CREAT|O_TRUNC,0644);
+		again = 1;}
+
+		
+	}while(again!=1);
+	
 
 	
 	
